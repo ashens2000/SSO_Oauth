5ca65a13d47c5f0549ce08eed24962d629833cd7
ashens2000 <73774945+ashens2000@users.noreply.github.com>
2022-10-31 17:34:43 +0530

    Final Commit on save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {OAuthService, JwksValidationHandler} from 'angular-oauth2-oidc';
+import { authConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sso_oauth';
+  
+  constructor(private oauthService:OAuthService)
+  {
+    this.configureSSO();
+  }
+  configureSSO()
+  {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler=new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndLogin();
+  }
+
+  // login(){
+
+  //   this.oauthService.initImplicitFlow();
+  // }
+  logout(){
+
+    this.oauthService.logOut();
+  }
+
+  get token()
+  {
+    let claims:any=this.oauthService.getIdentityClaims();
+    return claims ?claims :null;
+  }
 }
